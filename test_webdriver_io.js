d6c53633e34c388f8b1b18edfa62919fc92fc3c8
Michael Spiegel <spiagl.m@gmail.com>
2016-01-15 15:18:15 +0100

    Reformat code

--- test_webdriver_io.js
+++ test_webdriver_io.js
@@ -1,7 +1,11 @@
 "use strict";
 
+console.log('Custom test: Selenium Server + WebdriverIO');
+console.log('-----------------------------------------------------------');
+
+
 var webdriverio = require('webdriverio');
-var options = { desiredCapabilities: { browserName: 'chrome' } };
+var options = {desiredCapabilities: {browserName: 'chrome'}};
 var clientA = webdriverio.remote(options);
 var clientB = webdriverio.remote(options);
 
@@ -17,44 +21,44 @@ var browserB = clientB.init()
 
 var imageUrlA;
 
-browserA.url(baseUrl).then(function() {
+browserA.url(baseUrl).then(function () {
     console.log('A: initialized');
     // Wait until application is loaded
 
-}).getUrl().then(function(url) {
+}).getUrl().then(function (url) {
     console.log('A: url ' + url);
-    browserB.url(url).then(function() {
-        console.log('B: initialized');
+    browserB.url(url).then(function () {
+        console.log('B: url ' + url);
     });
-}).waitForVisible('h2.gallery-overview').then(function() {
+}).waitForVisible('h2.gallery-overview').then(function () {
     console.log('A: h2.gallery-overview is visible');
-}).click('//*[text()="Bike Tours"]').then(function() {
+}).click('//*[text()="Bike Tours"]').then(function () {
     console.log('A: clicked Bike Tours');
-}).pause(3000).then(function() {
+}).pause(3000).then(function () {
     console.log('A: waited for 3000ms');
-}).waitForVisible('#gallery img:nth-of-type(1)').then(function() {
+}).waitForVisible('#gallery img:nth-of-type(1)').then(function () {
     console.log('A: first image is visible');
-}) .click('#gallery img:nth-of-type(1)').then(function() {
+}).click('#gallery img:nth-of-type(1)').then(function () {
     console.log('A: clicked first image in galery');
-}).waitForVisible('#image img').then(function() {
+}).waitForVisible('#image img').then(function () {
     console.log('A: #image img is visible');
-}).getAttribute('#image img', 'src').then(function(src) {
+}).getAttribute('#image img', 'src').then(function (src) {
     imageUrlA = src;
     console.log('A: image src ' + src);
-}).getUrl().then(function(url) {
-    browserB.url(url).then(function() {
+}).getUrl().then(function (url) {
+    browserB.url(url).then(function () {
         console.log('B: loaded ' + url);
-    }).waitForExist('#image img').then(function() {
+    }).waitForExist('#image img').then(function () {
         console.log('B: image found');
-    }).pause(3000).then(function() {
+    }).pause(3000).then(function () {
         console.log('B: waited for 3000ms');
-    }).getAttribute('#image img', 'src').then(function(src) {
-            console.log('B: img src ' + src);
-            console.log('A: imageUrlA ' + imageUrlA);
-            if (src == imageUrlA) {
-                console.log('images are equal!');
-            } else {
-                console.log('ERROR! different images.');
-            }
+    }).getAttribute('#image img', 'src').then(function (src) {
+        console.log('A: imageUrlA ' + imageUrlA);
+        console.log('B: image src ' + src);
+        if (src == imageUrlA) {
+            console.log('SUCCESS, images are equal!');
+        } else {
+            console.log('ERROR! different images.');
+        }
     }).endAll();
 });
