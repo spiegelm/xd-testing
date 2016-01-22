78422aac9205dbb69e319d7a5f0f61f5e935deb6
Michael Spiegel <spiagl.m@gmail.com>
2016-01-22 18:13:57 +0100

    Specify browser session properties as test cases
    Test cookies are not shared across browser sessions.
    Test local storage is not shared across browser sessions.

--- test/first-test.js
+++ test/first-test.js
@@ -10,53 +10,109 @@ var webdriverio = require('webdriverio');
 
 var assert = buster.referee.assert;
 //var expect = buster.referee.expect;
-//var refute = buster.referee.refute;
+var refute = buster.referee.refute;
 
 //buster.spec.expose(); // Make BDD functions global
 
 buster.testCase("XD-MVC Example Gallery", {
 
-    setUp: function() {
+    setUp: function () {
 
         this.baseUrl = "http://me.local:8082/gallery.html";
 
         // Set timeout
-        this.timeout = 1000 * 60; // 60s
+        this.timeout = 1000 * 30; // 30s
 
         // New browser instance with WebdriverIO
         var options = {desiredCapabilities: {browserName: 'chrome'}};
         var clientA = webdriverio.remote(options);
         var clientB = webdriverio.remote(options);
 
-        var browserA = this.browserA = clientA.init()
+        this.browserA = clientA.init()
             .windowHandleSize({width: 800, height: 800})
             .windowHandlePosition({x: 0, y: 0});
-        var browserB = this.browserB = clientB.init()
+        this.browserB = clientB.init()
             .windowHandleSize({width: 800, height: 800})
             .windowHandlePosition({x: 800, y: 0});
 
     },
 
-    tearDown: function() {
+    tearDown: function () {
         // Close browser before completing a test
-        this.browserA.endAll();
-        //this.browserB.end();
-
-        return true;
+        this.browserA.end();
+        this.browserB.end();
     },
 
-    '//async test closes browser windows after test': function(done) {
-        setTimeout(function() {
+    '//async test closes browser windows after test': function (done) {
+        setTimeout(function () {
             assert.isTrue(true);
             done();
         }, 5000);
     },
 
-    'test second device shows selected image': function(done) {
-
+    'test cookies are not shared across browser sessions': function (done) {
         var self = this;
 
-        console.log("start");
+        self.browserA.url(this.baseUrl).then(function () {
+            self.browserB.url(self.baseUrl).then(function () {
+
+                self.browserA.setCookie({name: 'test_cookieA', value: 'A'})
+                    .getCookie('test_cookieA').then(function (cookie) {
+
+                    refute.isNull(cookie);
+                    assert.equals(cookie.name, 'test_cookieA');
+                    assert.equals(cookie.value, 'A');
+
+                    self.browserB.setCookie({name: 'test_cookieB', value: 'B'})
+                        .getCookie('test_cookieB').then(function (cookie) {
+
+                        refute.isNull(cookie);
+                        assert.equals(cookie.name, 'test_cookieB');
+                        assert.equals(cookie.value, 'B');
+
+                    }).getCookie('test_cookieA').then(function (cookie) {
+                        assert.isNull(cookie);
+
+                        self.browserA.endAll();
+                        done();
+                    });
+                });
+            });
+        });
+    },
+
+    'test local storage is not shared across browser sessions': function (done) {
+        var self = this;
+
+        self.browserA.url(this.baseUrl).then(function () {
+            self.browserB.url(self.baseUrl).then(function () {
+
+                var getItem = function(key) {
+                    console.log('localStorage.getItem ' + key);
+                    return localStorage.getItem(key);
+                };
+
+                var setItem = function(key, value) {
+                    localStorage.setItem(key, value);
+                };
+
+                self.browserA.execute(setItem, 'test_storageA', 'A').then(function(ret) {
+                }).execute(getItem, 'test_storageA').then(function (ret) {
+                    assert.equals(ret.value, 'A');
+
+                    self.browserB.execute(getItem, 'test_storageA').then(function (ret) {
+                        assert.isNull(ret.value);
+                        self.browserA.endAll();
+                        done();
+                    });
+                });
+            });
+        });
+    },
+
+    'test second device shows selected image': function (done) {
+
+        var self = this;
 
         var imageUrlA;
 
@@ -81,7 +137,7 @@ buster.testCase("XD-MVC Example Gallery", {
             console.log('A: clicked first image in galery');
         }).waitForVisible('#image img').then(function () {
             console.log('A: #image img is visible');
-        }).scroll(0, 1000).then(function() {
+        }).scroll(0, 1000).then(function () {
             console.log('A: scrolled down to the end');
         }).getAttribute('#image img', 'src').then(function (src) {
             imageUrlA = src;
@@ -112,35 +168,35 @@ buster.testCase("XD-MVC Example Gallery", {
 
                 // Tell test runner we're done with the async test
                 done();
-            });
+            }).endAll();
         });
     },
 
-    '//goes to Google': function() {
+    '//goes to Google': function () {
         var driver = this.webdriver.driver;
         var browser = this.browser;
 
-        return browser.url('http://www.google.com').then(function() {
+        return browser.url('http://www.google.com').then(function () {
             return browser.title();
-        }).then(function(title) {
+        }).then(function (title) {
             assert.equals(title, 'Google');
 
             return browser.elementByName('q');
-        }).then(function(input) {
+        }).then(function (input) {
             return input.type('webdriver');
-        }).then(function() {
+        }).then(function () {
             return browser.elementByName('btnG');
-        }).then(function(button) {
+        }).then(function (button) {
             return button.click();
-        }).then(function() {
+        }).then(function () {
             var defer = (driver.Q || browser.Q).defer();
 
             browser.waitForCondition('document.title === "webdriver - Google Search"', 5000, defer.resolve);
 
             return defer.promise;
-        }).then(function() {
+        }).then(function () {
             return browser.title();
-        }).then(function(title) {
+        }).then(function (title) {
             assert.equals(title, 'webdriver - Google Search');
         });
     }
