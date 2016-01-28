38746860c04874b79652519d3f3204860abc6d3e
Michael Spiegel <spiagl.m@gmail.com>
2016-01-28 18:32:58 +0100

    Improve support for multiple devices
    Execute an action on a set of devices.
    Retrieve the number of devices.

--- test/first-test.js
+++ test/first-test.js
@@ -7,6 +7,7 @@ if (typeof module == "object" && typeof require == "function") {
 
 var webdriverio = require('webdriverio');
 var q = require('q');
+//q.longStackSupport = true;
 
 
 var assert = buster.referee.assert;
@@ -49,8 +50,9 @@ var setupConnectedBrowsers = function() {
     var setup = q.defer();
 
     var self = this;
+    var deviceA = self.devices.select('A');
 
-    return self.browserA.url(self.baseUrl).then(function () {
+    return deviceA.url(self.baseUrl).then(function () {
         console.log('A: init');
     }).execute(function () {
         return 1 + 2;
@@ -62,20 +64,67 @@ var setupConnectedBrowsers = function() {
     }).execute(injectEventLogger).then(function() {
         console.log('A: injected event logger');
     }).getUrl().then(function(url) {
-        return self.browserB.url(url).then(function () {
-            console.log('B: init');
+        //return self.deviceB.url(url).then(function () {
 
-            return self.browserA.waitUntil(function() {
-                return self.browserA.execute(getEventCounter).then(function(ret) {
+        var allButA = Object.keys(self.deviceOptions).filter(function(deviceName) {
+            return deviceName != 'A';
+        });
+        return multiAction(self.devices, allButA, function(device) {
+            return device.url(url);
+        }).then(function() {
+            console.log('init urls');
+
+            return deviceA.waitUntil(function() {
+                return deviceA.execute(getEventCounter).then(function(ret) {
                     console.log('A: got eventCounter: ');
                     console.log(ret.value);
-                    return ret.value.XDconnection > 0;
+                    console.log('devices.length: ' + self.devicesCount());
+                    return ret.value.XDconnection == self.devicesCount() - 1;
+                    //return ret.value.XDconnection == devicesButA.count();
                 });
             });
         });
+
+        //var devicesButA = self.devices.select('B');
+        ////var devicesButA = self.devices.but('A');
+        //return devicesButA.url(url).then(function () {
+        //    console.log('but A: init');
+        //
+        //    return deviceA.waitUntil(function() {
+        //        return deviceA.execute(getEventCounter).then(function(ret) {
+        //            console.log('A: got eventCounter: ');
+        //            console.log(ret.value);
+        //            return ret.value.XDconnection > 0;
+        //            //return ret.value.XDconnection == devicesButA.count();
+        //        });
+        //    });
+        //});
     })
 
 };
+
+var devicesCount = function() {
+    var self = this;
+    return Object.keys(self.deviceOptions).length;
+};
+
+// Custom function: Do function/callback/promise for multiple browsers, except A
+
+/**
+ * Executes callback on devices matching deviceNames.
+ * Returns an promise.
+ * @param devices
+ * @param deviceNames
+ * @param callback
+ * @returns
+ */
+function multiAction (devices, deviceNames, callback) {
+    var promises = [];
+    deviceNames.forEach(function(name) {
+        promises.push(callback(devices.select(name)));
+    });
+    return q.all(promises);
+}
 
 
 //var specs = describe("XD-Test-gallery", function() {
@@ -87,30 +136,65 @@ buster.testCase("XD-MVC Example Gallery", {
 
         // Bind function to this reference
         this.setupConnectedBrowsers = setupConnectedBrowsers.bind(this);
+        this.devicesCount = devicesCount.bind(this);
 
         // Set timeout
         this.timeout = 1000 * 30; // 30s
 
         // New browser instance with WebdriverIO
-        var options = {desiredCapabilities: {browserName: 'chrome'}};
-        var clientA = webdriverio.remote(options);
-        var clientB = webdriverio.remote(options);
 
-        this.browserA = clientA.init();
-        this.browserA
-            .windowHandleSize({width: 800, height: 600})
-            .windowHandlePosition({x: 0, y: 0});
-        this.browserB = clientB.init()
-            .windowHandleSize({width: 800, height: 600})
-            .windowHandlePosition({x: 800, y: 0});
+
+        this.deviceOptions = {
+            A: {
+                desiredCapabilities: {browserName: 'chrome'}
+            },
+            B: {
+                desiredCapabilities: {browserName: 'chrome'}
+            },
+            C: {
+                desiredCapabilities: {browserName: 'chrome'}
+            }
+        };
+        this.devices = webdriverio.multiremote(this.deviceOptions);
+        //attachCustomFunctions(this.devices);
+
+
+
+
+        var self = this;
+
+        return this.devices.init()
+            .windowHandleSize({width: 500, height: 600})
+            .then(function() {
+                self.deviceA = self.devices.select('A');
+                self.deviceB = self.devices.select('B');
+            });
+
+
+        //var options = {desiredCapabilities: {browserName: 'chrome'}};
+        //var clientA = webdriverio.remote(options);
+        //var clientB = webdriverio.remote(options);
+
+        //this.deviceA = this.devices.select('A');
+        //this.deviceA
+        //    .windowHandleSize({width: 500, height: 600});
+        //    .windowHandlePosition({x: 0, y: 0});
+        //this.deviceB = this.devices.select('B');
+        //this.deviceB
+        //    .windowHandleSize({width: 500, height: 600})
+        //    .windowHandlePosition({x: 500, y: 0});
+        //this.deviceC = this.devices.select('C');
+        //this.deviceC
+        //    .windowHandleSize({width: 500, height: 600})
+        //    .windowHandlePosition({x: 1000, y: 0});
 
     },
 
     //after(function () {
     tearDown: function () {
         // Close browser before completing a test
-        this.browserA.end();
-        this.browserB.end();
+        return this.devices.end();
+        //this.deviceB.end();
     },
 
     // it('eventLogger', function() {
@@ -119,10 +203,10 @@ buster.testCase("XD-MVC Example Gallery", {
             var self = this;
 
             return self.setupConnectedBrowsers().then(function() {
-                return self.browserA.execute(getEventCounter).then(function(ret) {
+                return self.deviceA.execute(getEventCounter).then(function(ret) {
                     console.log('A: got eventCounter: ');
                     console.log(ret.value);
-                    expect(ret.value.XDconnection).toBe(1);
+                    expect(ret.value.XDconnection).toBe(self.devicesCount() - 1);
                 });
             });
         }
@@ -132,8 +216,8 @@ buster.testCase("XD-MVC Example Gallery", {
     'should not share cookies across browser sessions': function () {
         var self = this;
 
-        return self.browserA.url(this.baseUrl).then(function () {
-            return self.browserB.url(self.baseUrl);
+        return self.deviceA.url(this.baseUrl).then(function () {
+            return self.deviceB.url(self.baseUrl);
         }).setCookie({name: 'test_cookieA', value: 'A'})
         .getCookie('test_cookieA').then(function (cookie) {
 
@@ -141,7 +225,7 @@ buster.testCase("XD-MVC Example Gallery", {
             assert.equals(cookie.name, 'test_cookieA');
             assert.equals(cookie.value, 'A');
 
-            return self.browserB.setCookie({name: 'test_cookieB', value: 'B'})
+            return self.deviceB.setCookie({name: 'test_cookieB', value: 'B'})
             .getCookie('test_cookieB').then(function (cookie) {
 
                 refute.isNull(cookie);
@@ -168,20 +252,20 @@ buster.testCase("XD-MVC Example Gallery", {
             localStorage.setItem(key, value);
         };
 
-        return self.browserA.url(self.baseUrl).then(function () {
-            return self.browserB.url(self.baseUrl);
+        return self.deviceA.url(self.baseUrl).then(function () {
+            return self.deviceB.url(self.baseUrl);
         }).execute(setItem, 'test_storageA', 'A')
         .execute(getItem, 'test_storageA').then(function (ret) {
             assert.equals(ret.value, 'A');
 
-            return self.browserB.execute(getItem, 'test_storageA').then(function (ret) {
+            return self.deviceB.execute(getItem, 'test_storageA').then(function (ret) {
                 assert.isNull(ret.value);
             });
         });
     //});
     },
 
-    'should show the selected image on the second device': function () {
+    'should show the selected image on the other devices': function () {
     //it('should show the selected image on the second device', function () {
 
         var self = this;
@@ -206,7 +290,7 @@ buster.testCase("XD-MVC Example Gallery", {
             imageUrlA = src;
             console.log('A: image src ' + src);
         }).getUrl().then(function (url) {
-            return self.browserB.waitForVisible('#image img', 3000).then(function () {
+            return self.deviceB.waitForVisible('#image img', 3000).then(function () {
                 console.log('B: image found');
             }).getAttribute('#image img', 'src').then(function (src) {
                 console.log('A: imageUrlA ' + imageUrlA);
@@ -219,11 +303,11 @@ buster.testCase("XD-MVC Example Gallery", {
                 } else {
                     console.log('ERROR! different images.');
                 }
-            }).saveScreenshot('./screenshots/browserB.png', function (err, screenshot, response) {
+            }).saveScreenshot('./screenshots/deviceB.png', function (err, screenshot, response) {
                 console.log('B: save screenshot');
                 console.log('err: ' + err);
             });
-        }).saveScreenshot('./screenshots/browserA.png', function (err, screenshot, response) {
+        }).saveScreenshot('./screenshots/deviceA.png', function (err, screenshot, response) {
             console.log('A: save screenshot');
             console.log('err: ' + err);
         }).endAll();
