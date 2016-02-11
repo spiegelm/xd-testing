ad0c0307de8df4741e96b5432fa0897fb057411e
Michael Spiegel <spiagl.m@gmail.com>
2016-02-11 15:15:31 +0100

    Split CI scripts into setup and run

--- scripts/run_ci.sh
+++ scripts/run_ci.sh
@@ -5,34 +5,24 @@ set -u # Abort on uninitialized variable usage
 
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
-# Get XD-MVC
-cd ${PROJECT_ROOT}/testapp
-# This does not work in Windows because "file names are too long to delete"
-# rm -rf XD-MVC
-git clone --depth 1 https://github.com/mhusm/XD-MVC.git XD-MVC
-
-# Setup gallery
+# Run gallery
+echo "Run gallery app"
 cd ${PROJECT_ROOT}/testapp/XD-MVC/Examples/Gallery
-npm install
-bower install
-echo "Start gallery app"
-node gallery-polymer.js >/dev/null &
+node gallery-polymer.js &> /dev/null &
 GALLERY_PID=$!
 echo $GALLERY_PID
 
-# Setup maps
+# Run maps
+echo "Run gallery app"
 cd ${PROJECT_ROOT}/testapp/XD-MVC/Examples/Maps
-npm install
-bower install
-echo "Start gallery app"
-node server.js >/dev/null &
+node server.js &> /dev/null &
 MAPS_PID=$!
 echo $MAPS_PID
 
 # Setup selenium server
 cd ${PROJECT_ROOT}
 echo "Start selenium sever"
-./start_selenium.sh >/dev/null &
+./start_selenium.sh &> /dev/null &
 SELENIUM_PID=$!
 echo $SELENIUM_PID
 
@@ -41,7 +31,7 @@ echo "Run tests"
 npm test
 
 # Kill background processes
-set +e
+set +e # Ignore process kill errors
 kill $GALLERY_PID
 kill $MAPS_PID
 kill $SELENIUM_PID
---
+++ scripts/setup_ci.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e # Abort on error
+set -u # Abort on uninitialized variable usage
+
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+
+# Get XD-MVC
+cd ${PROJECT_ROOT}/testapp
+
+# Remove old repository
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Windows NT platform
+    # Deleting does not work in Windows because "file names are too long to delete"
+    DIRECTORY="${PROJECT_ROOT}/testapp/XD-MVC"
+    if [ -d "$DIRECTORY" ]; then
+        echo "You are running Windows. Please delete the folder '$DIRECTORY' manually."
+        echo "Press Return to continue..."
+        read
+    fi
+else
+    rm -rf XD-MVC
+fi
+
+# Clone XD-MVC repository
+git clone --depth 1 https://github.com/mhusm/XD-MVC.git XD-MVC
+
+# Setup gallery app
+echo "Setup gallery app"
+cd ${PROJECT_ROOT}/testapp/XD-MVC/Examples/Gallery
+npm install
+bower install
+
+# Setup maps app
+echo "Setup maps app"
+cd ${PROJECT_ROOT}/testapp/XD-MVC/Examples/Maps
+npm install
+bower install
