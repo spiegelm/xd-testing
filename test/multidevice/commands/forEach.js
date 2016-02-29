4bc28aa93ddfdce6f6e5b194d022b0168e7a0192
Michael Spiegel <spiagl.m@gmail.com>
2016-02-29 18:04:15 +0100

    Test forEach command

---
+++ test/multidevice/commands/forEach.js
@@ -0,0 +1,54 @@
+var assert = require('chai').assert;
+var utility = require('../../../lib/utility')
+var xdTesting = require('../../../lib/index')
+var templates = require('../../../lib/templates');
+
+describe('forEach', function () {
+    var self = this;
+
+    // Set test timeout
+    this.timeout(5 * 1000);
+    utility.waitforTimeout = 5 * 1000;
+
+    self.devices = {};
+    self.baseUrl = "http://localhost:8090/";
+
+    afterEach(function () {
+        // Close browsers before completing a test
+        return self.devices.endAll();
+    });
+
+
+    it('should call function on each device', function(done) {
+        var options = {A: templates.devices.chrome(), B: templates.devices.chrome()}
+        var counter = 0;
+        self.devices = xdTesting.multiremote(options)
+            .init()
+            .getCount().then(count => assert.equal(count, 2, 'getCount does not match'))
+            .forEach(function (device) {
+                counter++;
+            })
+            .then(function () {
+                assert.equal(counter, 2, 'has not been called twice');
+                done();
+            })
+        ;
+    });
+
+    //it('should return promises', function(done) {
+    //    var options = {A: templates.devices.chrome(), B: templates.devices.chrome()}
+    //    var counter = 0;
+    //    self.devices = xdTesting.multiremote(options)
+    //        .init()
+    //        .getCount().then(count => assert.equal(count, 2, 'getCount does not match'))
+    //        .forEach(function (device) {
+    //            console.log(device);
+    //            counter++;
+    //        })
+    //        .then(function () {
+    //            assert.equal(counter, 2, 'has not been called twice');
+    //            done();
+    //        })
+    //    ;
+    //});
+});
