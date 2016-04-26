137b2570a6d9b5f940c358cb105594dadb13d5c2
Michael Spiegel <spiagl.m@gmail.com>
2016-04-26 16:59:49 +0200

    Make Flow.sha() more robust and add tests

--- test/flow/flowTest.js
+++ test/flow/flowTest.js
@@ -7,6 +7,7 @@ var assert = require('chai').assert
 var fs = require('fs')
 var Flow = require('../../lib/flow/flow')
 var Device = require('../../lib/flow/device')
+var Checkpoint = require('../../lib/flow/checkpoint')
 var Step = require('../../lib/flow/step')
 var path = require('path')
 
@@ -26,6 +27,123 @@ describe('Flow @small', function () {
             assert.lengthOf(Object.keys(loadedFlow.devices), 1);
             assert.deepEqual(loadedFlow.toJSON(), flow.toJSON());
         });
+    })
+
+    describe('#sha', function () {
+        describe('returns equal hashes', function() {
+            it('for new flows', function() {
+                assert.equal(
+                    new Flow().sha(),
+                    new Flow().sha()
+                )
+            })
+            it('for equal flows with names', function() {
+                assert.equal(
+                    new Flow().setName('a').sha(),
+                    new Flow().setName('a').sha()
+                )
+            })
+            it('for equal devices without steps', function() {
+                assert.equal(
+                    new Flow().setName('a').addDevices([new Device({id: 'A'})]).sha(),
+                    new Flow().setName('a').addDevices([new Device({id: 'A'})]).sha()
+                )
+            })
+            it('for equal devices with steps without checkpoint', function() {
+                assert.equal(
+                    new Flow().setName('a').addDevices([new Device({id: 'A'}).addStep(new Step(0, 'command', null))]).sha(),
+                    new Flow().setName('a').addDevices([new Device({id: 'A'}).addStep(new Step(0, 'command', null))]).sha()
+                )
+            })
+            it('for equal devices with checkpoints and steps', function() {
+                assert.equal(
+                    new Flow().setName('a').addDevices([
+                        new Device({id: 'A'}).addCheckpoint(
+                            new Checkpoint(0, 'checkpoint').addSteps([
+                                new Step(0, 'command0', null),
+                                new Step(1, 'command1', null)
+                            ])
+                        )
+                    ]).sha(),
+                    new Flow().setName('a').addDevices([
+                        new Device({id: 'A'}).addCheckpoint(
+                            new Checkpoint(0, 'checkpoint').addSteps([
+                                new Step(0, 'command0', null),
+                                new Step(1, 'command1', null)
+                            ])
+                        )
+                    ]).sha()
+                )
+            })
+            it('for equal devices with checkpoints and steps', function() {
+                assert.equal(
+                    new Flow().setName('a').addDevices([
+                        new Device({id: 'A'}).addCheckpoint(
+                            new Checkpoint(0, 'checkpoint').addSteps([
+                                new Step(0, 'command0', new Buffer([13, 37])),
+                                new Step(1, 'command1', new Buffer([13, 37]))
+                            ])
+                        )
+                    ]).sha(),
+                    new Flow().setName('a').addDevices([
+                        new Device({id: 'A'}).addCheckpoint(
+                            new Checkpoint(0, 'checkpoint').addSteps([
+                                new Step(0, 'command0', new Buffer([13, 37])),
+                                new Step(1, 'command1', new Buffer([13, 37]))
+                            ])
+                        )
+                    ]).sha()
+                )
+            })
+        })
+
+        describe('returns different hashes', function() {
+            it('for flows with different names', function() {
+                assert.notEqual(
+                    new Flow().setName('b').sha(),
+                    new Flow().setName('a').sha()
+                )
+            })
+            it('for flows with different devices', function() {
+                assert.notEqual(
+                    new Flow().addDevices([new Device({id: 'A'})]).sha(),
+                    new Flow().addDevices([new Device({id: 'B'})]).sha()
+                )
+            })
+            it('for flows with different step count', function() {
+                assert.notEqual(
+                    new Flow().addDevices([new Device({id: 'A'}).addStep(new Step(0, 'command', null))]).sha(),
+                    new Flow().addDevices([new Device({id: 'B'})]).sha()
+                )
+            })
+            it('for flows with different steps', function() {
+                assert.notEqual(
+                    new Flow().addDevices([new Device({id: 'A'}).addStep(new Step(0, 'command', null))]).sha(),
+                    new Flow().addDevices([new Device({id: 'B'}).addStep(new Step(1, 'command', null))]).sha()
+                )
+            })
+            it('for devices different step images', function() {
+                assert.notEqual(
+                    new Flow().setName('a').addDevices([
+                        new Device({id: 'A'}).addCheckpoint(
+                            new Checkpoint(0, 'checkpoint').addSteps([
+                                new Step(0, 'command0', new Buffer([13, 37])),
+                                new Step(1, 'command1', new Buffer([13, 37]))
+                            ])
+                        )
+                    ]).sha(),
+                    new Flow().setName('a').addDevices([
+                        new Device({id: 'A'}).addCheckpoint(
+                            new Checkpoint(0, 'checkpoint').addSteps([
+                                new Step(0, 'command0', new Buffer([13, 37])),
+                                new Step(1, 'command1', new Buffer([13, 39]))
+                            ])
+                        )
+                    ]).sha()
+                )
+            })
+
+        })
     })
 
     describe('#addDevice', function () {
@@ -100,12 +218,12 @@ describe('Flow @small', function () {
     })
 
     describe('#grid', function() {
-        it('x test', function() {
+        it('x test' /*, function() {
             let FLOW_TEST_FILE = path.join(__dirname, 'fixtures', 'flow.json')
             Flow.load(FLOW_TEST_FILE).grid()
 
             // TODO finish test
-        })
+        }*/)
     })
 
     describe('#generateStepId', function() {
