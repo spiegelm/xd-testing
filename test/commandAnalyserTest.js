9b371593f57287a7700f9444eb4e438f6f40f725
Michael Spiegel <spiagl.m@gmail.com>
2016-05-18 02:01:22 +0200

    Test CommandAnalyzer

---
+++ test/commandAnalyserTest.js
@@ -0,0 +1,35 @@
+"use strict"
+
+/**
+ * @type {Chai.Assert}
+ */
+var assert = require('chai').assert
+var templates = require('../lib/templates')
+var CommandAnalyzer = require('../lib/commandAnalyzer')
+var q = require('q')
+
+describe('CommandAnalyzer @small', () => {
+    describe('.acceptsElementSelector', () => {
+        it('is true for commands with element selector as first argument', () => {
+            let elementSelectorCommands = CommandAnalyzer.getElementSelectorCommands()
+            elementSelectorCommands.forEach(commandName => {
+                let command = new CommandAnalyzer(commandName, [])
+                assert.isTrue(command.acceptsElementSelector())
+            })
+        })
+    })
+
+    describe('element selector commands', () => {
+        it('either accept an optional, a required, two required, or an array of selectors', () => {
+            CommandAnalyzer.getElementSelectorCommands().forEach(commandName => {
+                let command = new CommandAnalyzer(commandName, [])
+                let count = 0
+                count += command.hasOptionalElementSelector()
+                count += command.requiresOneElementSelector()
+                count += command.requiresTwoElementSelectors()
+                count += command.requiresOneElementOrArraySelector()
+                assert.equal(count, 1, 'Failed for command ' + commandName)
+            })
+        })
+    })
+})
