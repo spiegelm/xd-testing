5364a6e609031d2e07f2041178a40a5a4a193970
Michael Spiegel <spiagl.m@gmail.com>
2016-05-20 17:45:20 +0200

    Document missing element related commands

--- test/commandAnalyserTest.js
+++ test/commandAnalyserTest.js
@@ -10,7 +10,7 @@ var q = require('q')
 
 describe('CommandAnalyzer @small', () => {
     describe('.acceptsElementSelector', () => {
-        it('is true for commands with element selector as first argument', () => {
+        it('is true for commands with optional or required element selector as first argument', () => {
             let elementSelectorCommands = CommandAnalyzer.getElementSelectorCommands()
             elementSelectorCommands.forEach(commandName => {
                 let command = new CommandAnalyzer(commandName, [])
@@ -19,7 +19,7 @@ describe('CommandAnalyzer @small', () => {
         })
     })
 
-    describe('element selector commands', () => {
+    describe('element selector commands', function () {
         it('either accept an optional, a required, two required, or an array of selectors', () => {
             CommandAnalyzer.getElementSelectorCommands().forEach(commandName => {
                 let command = new CommandAnalyzer(commandName, [])
@@ -30,6 +30,20 @@ describe('CommandAnalyzer @small', () => {
                 count += command.requiresOneElementOrArraySelector()
                 assert.equal(count, 1, 'Failed for command ' + commandName)
             })
+        })
+
+        it('are part of at least one category', function () {
+            let commands = CommandAnalyzer.getElementSelectorCommands()
+            let missingCommands = CommandAnalyzer.getAllElementSelectorCommands().filter(commandName => {
+                return commands.indexOf(commandName) === -1
+            })
+            if (missingCommands) {
+                console.log('missing commands:')
+                missingCommands.forEach(commandName => console.log(commandName))
+            }
+            this.skip()
+            // TODO ensure all element related commands are covered
+            assert.lengthOf(missingCommands, 0, "Missing commands")
         })
     })
 })
