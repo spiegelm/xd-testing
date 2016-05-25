55791d574f12842fcd957c16dbcd72eae062ec32
Michael Spiegel <spiagl.m@gmail.com>
2016-05-25 13:46:12 +0200

    Test that xdTesting.baseUrl is loaded after init

--- test/xd-testing/xdTestingTest.js
+++ test/xd-testing/xdTestingTest.js
@@ -12,8 +12,8 @@ describe('xdTesting @large', function() {
 
     let baseUrl = 'http://localhost:8090/'
 
-    describe('command arguments', function() {
-        it('are part of a step\'s command name', function() {
+    describe('command arguments', function () {
+        it('are part of a step\'s command name', function () {
             let scenario = {A: templates.devices.chrome()}
             return xdTesting.multiremote(scenario).init()
                 .url(baseUrl)
@@ -29,8 +29,8 @@ describe('xdTesting @large', function() {
         })
     })
 
-    describe('#getFlow', function() {
-        it('returns the flow object', function() {
+    describe('#getFlow', function () {
+        it('returns the flow object', function () {
             let scenario = {A: templates.devices.chrome()}
             return xdTesting.multiremote(scenario).init()
                 .url(baseUrl)
@@ -44,5 +44,47 @@ describe('xdTesting @large', function() {
                 .end()
 
         })
+    })
+
+
+    describe('app framework integration', () => {
+
+        describe('for XD-MVC', () => {
+            //// Set base url
+            xdTesting.baseUrl = 'http://localhost/'
+
+            // Define a custom adapter
+            xdTesting.app = xdTesting.adapter.xdmvc
+
+            it('should load xdTesting.baseUrl after init', () => {
+                let options = {
+                    A: templates.devices.chrome()
+                }
+
+                // Enable app specific commands
+                xdTesting.app = xdTesting.adapter.xdmvc()
+
+                return xdTesting.multiremote(options).init()
+                    .getUrl().then(url => assert.equal(url, xdTesting.baseUrl))
+
+                    /**
+                     * App property?
+                     */
+
+                    //// Pair devices via url
+                    //.app.pairDevicesViaUrl()
+                    // Get event counter
+                    //.app.getEventCounter().then(counter => assert.equal(counter.XDconnection == 1))
+                // Wait for the next event of a type
+                //.app.waitForEvent('XDconnection')
+                //// Wait until the event type counter equals a value
+                //.app.waitForEventCount('XDconnection', 2)
+                //// Custom command
+                //.app.addCommand('pairDevicesViaGUI', function() {
+                //    return this.click(...)
+                //})
+            })
+        })
+
     })
 })
