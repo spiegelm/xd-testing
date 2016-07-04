5564417ba0691becb7ca24af198b18e5ae6733bb
Michael Spiegel <spiagl.m@gmail.com>
2016-07-04 14:21:25 +0200

    Test `selectAny` on empty device set

--- test/multidevice/selectAny.js
+++ test/multidevice/selectAny.js
@@ -7,12 +7,12 @@ var q = require('q')
 
 describe('MultiDevice - selectAny', function () {
     it('should select a single device @medium', function () {
-        var options = {
+        var scenario = {
             A: templates.devices.chrome(),
             B: templates.devices.chrome()
         }
 
-        return xdTesting.multiremote(options)
+        return xdTesting.multiremote(scenario)
             .getCount().then(count => assert.equal(count, 2))
             .selectAny(device => device
                 .getCount().then(count => assert.equal(count, 1))
@@ -20,13 +20,13 @@ describe('MultiDevice - selectAny', function () {
     })
 
     it('should execute promises callback @medium', function () {
-        var options = {
+        var scenario = {
             A: templates.devices.chrome(),
             B: templates.devices.chrome()
         }
 
         var queue = ''
-        return xdTesting.multiremote(options)
+        return xdTesting.multiremote(scenario)
             .then(() => queue += '0')
             .selectAny(device => {
                 queue += '1'
@@ -38,4 +38,24 @@ describe('MultiDevice - selectAny', function () {
             .then(() => assert.equal(queue, '0123'))
     })
 
+    describe('on empty device sets', function() {
+
+        it('should throw an error on command execution', function() {
+            var scenario = {
+            }
+
+            var queue = ''
+            return xdTesting.multiremote(scenario)
+                .then(() => queue += '0')
+                .selectAny(device => device
+                    .click('#button')
+                )
+                .then(result => {
+                    throw new Error('Promise was unexpectedly fulfilled. Result: ' + result)
+                }, err => {
+                    assert.instanceOf(err, Error)
+                    assert.match(err.message, /not defined/)
+                })
+        })
+    })
 })
